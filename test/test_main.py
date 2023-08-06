7fbd34c09931131a6e7e78ea2ccd458e88fd2047
snazeri2009 <s.nazeri2009@gmail.com>
2023-08-06 20:59:59 +0300

    add test

--- test/test_main.py
+++ test/test_main.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from main import hello_world, hello_Goodby
+from main import hello_world, Goodby_world
 
 
 def test_hello_world():
@@ -12,6 +12,6 @@ def test_hello_world():
     assert result == "Hello, World!"
 
 
-def test_hello_Goodby():
-    result = hello_world()
+def test_Goodby_world():
+    result = Goodby_world()
     assert result == "Goodby, World!"
