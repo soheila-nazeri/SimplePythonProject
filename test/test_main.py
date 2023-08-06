6de9cdad4a4528fd1e0c415cab5adde2c4da9cf2
snazeri2009 <s.nazeri2009@gmail.com>
2023-08-06 16:42:23 +0300

    os init

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,11 @@
-from ..main import hello_world
+# File: test/test_main.py
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from main import hello_world
 
 
 def test_hello_world():
